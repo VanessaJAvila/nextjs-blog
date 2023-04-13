d1ea309b10e51589291bd1287b23cae321aaf923
Vanessa Avila <vanessa.3bl@gmail.com>
2023-04-13 11:25:01 +0100

    update

---
+++ src/pages/api/hello.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  const email = req.body.email;
+  // Then save email to your database, etc...
+}
